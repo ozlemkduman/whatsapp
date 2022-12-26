4c984dac6935ff9e256c38c10436a76381aa3ec9
ozlemkduman <ozlemk.duman@gmail.com>
2022-12-26 09:13:59 +0300

    iki key hali

--- script.js
+++ script.js
@@ -6,28 +6,44 @@ const buttonChevron = document.querySelector(".buttonChevron");
 const main = document.querySelector(".main");
 
 const message = document.querySelector(".message");
-const youMessage = document.querySelector(".youMessage");
+let youMessage = document.querySelector(".youMessage");
 
-const messageList=document.querySelector(".messageList");
-
-input.addEventListener("click" ,function(){
-    camera.style.display="none";
-    buttonMicrophone.style.display="none";
-    buttonChevron.style.display="inline";
+let messageList = document.querySelector(".messageList");
+let youMessageList = document.querySelector(".youMessageList");
+input.addEventListener("click", function () {
+    camera.style.display = "none";
+    buttonMicrophone.style.display = "none";
+    buttonChevron.style.display = "inline";
 })
 
 input.addEventListener("keypress", function (event) {
 
+
+
     if (event.key === "Enter") {
-        console.log("yapıldı");
-        let textMessageLi=document.createElement("li");
+        event.preventDefault();
+        let textMessageLi = document.createElement("li");
+        textMessageLi.classList.add("textMessageLi")
         messageList.appendChild(textMessageLi);
         textMessageLi.classList.add("textMessageLiStyle");
-        localStorage.setItem("input",input.value);
-        textMessageLi.innerHTML=input.value;
+        localStorage.setItem("input", input.value);
+        textMessageLi.innerHTML = input.value;
         console.log(textMessageLi.innerHTML)
         messageList.classList.add("messageListStyle");
-        input.value="";
+        input.value = "";
+    }
 
+
+
+    else if(event.key==="a"){ event.preventDefault();
+      let youTextMessageLi = document.createElement("li");
+      youTextMessageLi.classList.add("youTextMessageLi")
+      youMessageList.appendChild(youTextMessageLi);
+      youTextMessageLi.classList.add("youTextMessageLiStyle");
+      localStorage.setItem("input", input.value);
+      youTextMessageLi.innerHTML = input.value;
+      console.log(youTextMessageLi.innerHTML)
+      youMessageList.classList.add("youMessageListStyle");
+      input.value = "";
     }
 });
