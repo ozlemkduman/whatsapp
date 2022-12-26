d3f7e00e4f34c4231ca1ed3d2545d54c91b0562d
ozlemkduman <ozlemk.duman@gmail.com>
2022-12-26 10:49:36 +0300

    proje bitti

--- script.js
+++ script.js
@@ -16,34 +16,49 @@ input.addEventListener("click", function () {
     buttonChevron.style.display = "inline";
 })
 
+let sayac = true;
+
 input.addEventListener("keypress", function (event) {
 
 
 
     if (event.key === "Enter") {
-        event.preventDefault();
-        let textMessageLi = document.createElement("li");
-        textMessageLi.classList.add("textMessageLi")
-        messageList.appendChild(textMessageLi);
-        textMessageLi.classList.add("textMessageLiStyle");
-        localStorage.setItem("input", input.value);
-        textMessageLi.innerHTML = input.value;
-        console.log(textMessageLi.innerHTML)
-        messageList.classList.add("messageListStyle");
-        input.value = "";
+
+        if (sayac == true) {
+            event.preventDefault();
+            let messageList=document.createElement("ul");
+            main.appendChild(messageList);
+            messageList.classList.add("messageListStyle");
+            let textMessageLi = document.createElement("li");
+            textMessageLi.classList.add("textMessageLi")
+            messageList.appendChild(textMessageLi);
+            textMessageLi.classList.add("textMessageLiStyle");
+            textMessageLi.innerHTML = input.value;
+            console.log(textMessageLi.innerHTML)
+            messageList.classList.add("messageListStyle");
+            input.value = "";
+            sayac = false;
+        } else {
+            event.preventDefault();
+            let youMessageList=document.createElement("ul");
+            main.appendChild(youMessageList);
+            youMessageList.classList.add("youMessageListStyle");
+            let youTextMessageLi = document.createElement("li");
+            youTextMessageLi.classList.add("youTextMessageLi")
+            youMessageList.appendChild(youTextMessageLi);
+            youTextMessageLi.classList.add("youTextMessageLiStyle");
+            localStorage.setItem("input", input.value);
+            youTextMessageLi.innerHTML = input.value;
+            console.log(youTextMessageLi.innerHTML)
+            youMessageList.classList.add("youMessageListStyle");
+            input.value = "";
+            sayac = true;
+        }
     }
 
 
 
-    else if(event.key==="a"){ event.preventDefault();
-      let youTextMessageLi = document.createElement("li");
-      youTextMessageLi.classList.add("youTextMessageLi")
-      youMessageList.appendChild(youTextMessageLi);
-      youTextMessageLi.classList.add("youTextMessageLiStyle");
-      localStorage.setItem("input", input.value);
-      youTextMessageLi.innerHTML = input.value;
-      console.log(youTextMessageLi.innerHTML)
-      youMessageList.classList.add("youMessageListStyle");
-      input.value = "";
+    else if (event.key === "a") {
+
     }
 });
