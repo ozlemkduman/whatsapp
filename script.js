9bcc3d1324e1c61fdf07f15827dd88a7f19692bf
ozlemkduman <ozlemk.duman@gmail.com>
2022-12-27 12:09:45 +0300

    local storage veri alma yazdırma deneme

--- script.js
+++ script.js
@@ -10,55 +10,96 @@ let youMessage = document.querySelector(".youMessage");
 
 let messageList = document.querySelector(".messageList");
 let youMessageList = document.querySelector(".youMessageList");
+
+
+let textLeft=document.querySelector(".textLeft");
+let textRight=document.querySelector(".textRight");
+
+let infoOfPageLeft=localStorage.getItem(itemLeft);
+let infoOfPageRight=localStorage.getItem(itemRight);;
+
+
+
+
+textLeft.innerHTML=infoOfPageLeft;
+textRight.innerHTML=infoOfPageRight;
+
+
 input.addEventListener("click", function () {
     camera.style.display = "none";
     buttonMicrophone.style.display = "none";
     buttonChevron.style.display = "inline";
 })
 
-let sayac = true;
+let isLastMessageOnLeft = true;
 
 input.addEventListener("keypress", function (event) {
 
-
-
     if (event.key === "Enter") {
-
-        if (sayac == true) {
-            event.preventDefault();
-            let messageList=document.createElement("ul");
-            main.appendChild(messageList);
-            messageList.classList.add("messageListStyle");
-            let textMessageLi = document.createElement("li");
-            textMessageLi.classList.add("textMessageLi")
-            messageList.appendChild(textMessageLi);
-            textMessageLi.classList.add("textMessageLiStyle");
-            textMessageLi.innerHTML = input.value;
-            console.log(textMessageLi.innerHTML)
-            messageList.classList.add("messageListStyle");
-            input.value = "";
-            sayac = false;
-        } else {
-            event.preventDefault();
-            let youMessageList=document.createElement("ul");
-            main.appendChild(youMessageList);
-            youMessageList.classList.add("youMessageListStyle");
-            let youTextMessageLi = document.createElement("li");
-            youTextMessageLi.classList.add("youTextMessageLi")
-            youMessageList.appendChild(youTextMessageLi);
-            youTextMessageLi.classList.add("youTextMessageLiStyle");
-            localStorage.setItem("input", input.value);
-            youTextMessageLi.innerHTML = input.value;
-            console.log(youTextMessageLi.innerHTML)
-            youMessageList.classList.add("youMessageListStyle");
-            input.value = "";
-            sayac = true;
-        }
-    }
-
-
-
-    else if (event.key === "a") {
+        send()
 
     }
 });
+
+buttonChevron.addEventListener("click", send);
+
+
+
+
+function send() {
+    if (isLastMessageOnLeft == true) {
+        addMessageOnLeft();
+
+    } else {
+
+        addMessageOnRight();
+    }
+};
+
+function itemRight(){
+    let itemR= localStorage.setItem("input",input.value);
+     
+    return itemR;
+}
+
+function itemLeft(){
+    let itemL= localStorage.setItem("input",input.value);
+
+    return itemL;
+    
+}
+
+
+function addMessageOnLeft() {
+    itemRight()
+    let messageList = document.createElement("ul");
+    main.appendChild(messageList);
+    messageList.classList.add("messageListStyle");
+    let textMessageLi = document.createElement("li");
+    textMessageLi.classList.add("textMessageLi")
+    messageList.appendChild(textMessageLi);
+    textMessageLi.classList.add("textMessageLiStyle");
+    textMessageLi.innerHTML = input.value;
+    console.log(itemRight())
+    messageList.classList.add("messageListStyle");
+    input.value = "";
+    isLastMessageOnLeft = false;
+
+
+}
+
+
+function addMessageOnRight() {
+    itemLeft()
+    let youMessageList = document.createElement("ul");
+    main.appendChild(youMessageList);
+    youMessageList.classList.add("youMessageListStyle");
+    let youTextMessageLi = document.createElement("li");
+    youTextMessageLi.classList.add("youTextMessageLi")
+    youMessageList.appendChild(youTextMessageLi);
+    youTextMessageLi.classList.add("youTextMessageLiStyle");
+    youTextMessageLi.innerHTML = input.value;
+    youMessageList.classList.add("youMessageListStyle");
+    input.value = "";
+    isLastMessageOnLeft = true;
+}
